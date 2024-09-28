5db0cc71f37f10fe964edbc421811f4729b17a72
Dhanegave Samir <165806543+SamirDhanegave@users.noreply.github.com>
2024-09-28 18:34:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function onWindowResize() {
+    camera.aspect = container.offsetWidth / container.offsetHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
